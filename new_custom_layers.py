3141f19c90983b512cf546298908c438a608ca1e
sevvi <joseph.hosanna@gmail.com>
2019-03-02 14:35:47 +0200

    made changes

--- new_custom_layers.py
+++ new_custom_layers.py
@@ -29,6 +29,171 @@ from tensorflow_probability.python.layers import util as tfp_layers_util
 from tensorflow_probability.python.math import random_rademacher
 from tensorflow_probability.python.util import docstring as docstring_util
 from tensorflow_probability.python.layers.dense_variational import _DenseVariational
+class LCSoftmax(_DenseVariational):
+  # @docstring_util.expand_docstring(args=doc_args)
+  def __init__(
+      self,
+      units,
+      util_mat,
+      activation=None,
+      activity_regularizer=None,
+      trainable=True,
+      kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
+      kernel_posterior_tensor_fn=lambda d: d.sample(),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
+      kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
+      bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(
+          is_singular=True),
+      bias_posterior_tensor_fn=lambda d: d.sample(),
+      bias_prior_fn=None,
+      bias_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
+      **kwargs):
+    # pylint: disable=g-doc-args
+    """Construct layer.
+    Args:
+      ${args}
+    """
+    # pylint: enable=g-doc-args
+    super(LCSoftmax, self).__init__(
+        units=units,
+        activation=activation,
+        activity_regularizer=activity_regularizer,
+        trainable=trainable,
+        kernel_posterior_fn=kernel_posterior_fn,
+        kernel_posterior_tensor_fn=kernel_posterior_tensor_fn,
+        kernel_prior_fn=kernel_prior_fn,
+        kernel_divergence_fn=kernel_divergence_fn,
+        bias_posterior_fn=bias_posterior_fn,
+        bias_posterior_tensor_fn=bias_posterior_tensor_fn,
+        bias_prior_fn=bias_prior_fn,
+        bias_divergence_fn=bias_divergence_fn,
+        **kwargs)
+    self.util_mat =  tf.convert_to_tensor(value=util_mat, dtype=tf.float32)
+    self.input_spec = [ tf.keras.layers.InputSpec(min_ndim=2),
+                        tf.keras.layers.InputSpec(min_ndim=1),
+                        tf.keras.layers.InputSpec(min_ndim=1)]
+
+  def build(self, input_shape_list):
+    assert len(input_shape_list) == 3
+    input_shape, target_shape, hypothesis_shape = input_shape_list
+    input_shape = tf.TensorShape(input_shape)
+    target_shape = tf.TensorShape(target_shape)
+    hypothesis_shape = tf.TensorShape(hypothesis_shape)
+    in_size = input_shape[-1].value
+    if in_size is None:
+      raise ValueError('The last dimension of the inputs to `Dense` '
+                       'should be defined. Found `None`.')
+    # self._input_spec = tf.keras.layers.InputSpec(min_ndim=2, axes={-1: in_size})
+    self.input_spec = [tf.keras.layers.InputSpec(min_ndim=2,
+                                axes={-1: in_size}),
+                       tf.keras.layers.InputSpec(min_ndim=1,
+                                axes={-1: target_shape[-1].value}),
+                        tf.keras.layers.InputSpec(min_ndim=1,
+                                 axes={-1: hypothesis_shape[-1].value})]
+
+    # If self.dtype is None, build weights using the default dtype.
+    dtype = tf.as_dtype(self.dtype or tf.keras.backend.floatx())
+
+    # Must have a posterior kernel.
+    self.kernel_posterior = self.kernel_posterior_fn(
+        dtype, [self.units, in_size], 'kernel_posterior',
+        self.trainable, self.add_variable)
+
+    if self.kernel_prior_fn is None:
+      self.kernel_prior = None
+    else:
+      self.kernel_prior = self.kernel_prior_fn(
+          dtype, [self.units, in_size], 'kernel_prior',
+          self.trainable, self.add_variable)
+    self._built_kernel_divergence = False
+
+    if self.bias_posterior_fn is None:
+      self.bias_posterior = None
+    else:
+      self.bias_posterior = self.bias_posterior_fn(
+          dtype, [self.units], 'bias_posterior',
+          self.trainable, self.add_variable)
+
+    if self.bias_prior_fn is None:
+      self.bias_prior = None
+    else:
+      self.bias_prior = self.bias_prior_fn(
+          dtype, [self.units], 'bias_prior',
+          self.trainable, self.add_variable)
+    self._built_bias_divergence = False
+
+    self.built = True
+
+  def call(self, inputs_list):
+    inputs, targets, hypothesis = inputs_list
+    inputs = tf.convert_to_tensor(value=inputs, dtype=self.dtype)
+    targets = tf.convert_to_tensor(value=targets, dtype=self.dtype)
+    hypothesis = tf.convert_to_tensor(value=hypothesis, dtype=self.dtype)
+
+    hypothesis = math_ops.cast(hypothesis, dtypes.int64)
+    hypothesis_flat = array_ops.reshape(hypothesis, [-1])
+
+
+    self.kernel_posterior_tensor = self.kernel_posterior_tensor_fn(
+        self.kernel_posterior)
+    self.kernel_posterior_affine = None
+    self.kernel_posterior_affine_tensor = None
+    # outputs = tf.matmul(inputs, self.kernel_posterior_tensor, transpose_b=True)
+    self.bias_posterior_tensor = self.bias_posterior_tensor_fn(
+        self.bias_posterior)
+    # logits = tf.nn.bias_add(outputs, self.bias_posterior_tensor)
+
+    sampled_values = candidate_sampling_ops.all_candidate_sampler(
+                            true_classes = tf.cast(targets, tf.int64),
+                            num_true = 1,
+                            num_sampled = self.units,
+                            unique = True)
+
+    true_logits, sampled_logits = _compute_sampled_logits(
+                    self.kernel_posterior_tensor,
+                    self.bias_posterior_tensor,
+                    targets, inputs, self.units, self.units,
+                    sampled_values = sampled_values,
+                    remove_accidental_hits = False)
+
+    log_Z = tf.reduce_logsumexp(sampled_logits, 1)
+    softmax_loss = - true_logits + log_Z
+
+    softmax_probs = tf.exp(sampled_logits  -  log_Z)
+    util = tf.nn.embedding_lookup(self.util_mat, hypothesis_flat)
+    gain = _sum_rows(util * softmax_probs)
+    lc_loss = softmax_loss - tf.log(gain)
+    lc_loss = tf.keras.backend.mean(lc_loss)
+
+    self.add_loss(lc_loss)
+
+    if not self._built_kernel_divergence:
+      self._apply_divergence(self.kernel_divergence_fn,
+                             self.kernel_posterior,
+                             self.kernel_prior,
+                             self.kernel_posterior_tensor,
+                             name='divergence_kernel')
+      self._built_kernel_divergence = True
+    if not self._built_bias_divergence:
+      self._apply_divergence(self.bias_divergence_fn,
+                             self.bias_posterior,
+                             self.bias_prior,
+                             self.bias_posterior_tensor,
+                             name='divergence_bias')
+      self._built_bias_divergence = True
+    return outputs
+
+  def compute_output_shape(self, input_shape):
+    assert input_shape and len(input_shape) == 3
+    assert input_shape[0][-1]
+    input_shape = tf.TensorShape(input_shape)
+    input_shape = input_shape.with_rank_at_least(2)
+    if tf.compat.dimension_value(input_shape[-1]) is None:
+      raise ValueError(
+          'The innermost dimension of `input_shape` must be defined, '
+          'but saw: {}'.format(input_shape))
+    return input_shape[:-1].concatenate(self.units)
+
 
 class ARLayer(_DenseVariational):
   # @docstring_util.expand_docstring(args=doc_args)
@@ -181,6 +346,7 @@ class ARLayer(_DenseVariational):
           'The innermost dimension of `input_shape` must be defined, '
           'but saw: {}'.format(input_shape))
     return input_shape[:-1].concatenate(self.units)
+
 class OVELayer(_DenseVariational):
   # @docstring_util.expand_docstring(args=doc_args)
   def __init__(
@@ -325,6 +491,7 @@ class OVELayer(_DenseVariational):
           'The innermost dimension of `input_shape` must be defined, '
           'but saw: {}'.format(input_shape))
     return input_shape[:-1].concatenate(self.units)
+
 class Softmax(_DenseVariational):
   # @docstring_util.expand_docstring(args=doc_args)
   def __init__(
@@ -482,6 +649,7 @@ class Softmax(_DenseVariational):
     self.kernel_posterior_affine = None
     self.kernel_posterior_affine_tensor = None
     return tf.matmul(inputs, self.kernel_posterior_tensor, transpose_b=True)
+
 def _compute_sampled_logits(weights,
                             biases,
                             labels,
@@ -658,6 +826,7 @@ def _compute_sampled_logits(weights,
     # ], 1)
 
     return true_logits, sampled_logits
+
 def _sum_rows(x):
   """Returns a vector summing up each row of the matrix x."""
   # _sum_rows(x) is equivalent to math_ops.reduce_sum(x, 1) when x is
