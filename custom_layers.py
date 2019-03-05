82f65691868fa7bbfa1ec5c618fe49057c630c8a
sevvi <joseph.hosanna@gmail.com>
2019-03-05 11:54:36 +0200

    added tfsoftmax

--- custom_layers.py
+++ custom_layers.py
@@ -359,3 +359,16 @@ def _compute_sampled_logits(weights,
     # ], 1)
 
     return true_logits, sampled_logits
+
+
+def _sum_rows(x):
+  """Returns a vector summing up each row of the matrix x."""
+  # _sum_rows(x) is equivalent to math_ops.reduce_sum(x, 1) when x is
+  # a matrix.  The gradient of _sum_rows(x) is more efficient than
+  # reduce_sum(x, 1)'s gradient in today's implementation. Therefore,
+  # we use _sum_rows(x) in the nce_loss() computation since the loss
+  # is mostly used for training.
+  cols = array_ops.shape(x)[1]
+  ones_shape = array_ops.stack([cols, 1])
+  ones = array_ops.ones(ones_shape, x.dtype)
+  return array_ops.reshape(math_ops.matmul(x, ones), [-1])
